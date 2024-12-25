35fac22498f866172550c98737d8f4992eca5ec8
Christophe SAUVEUR <chsxf.pro@gmail.com>
2024-12-25 23:06:55 +0100

    ✨ Sharing

---
+++ js/share.js
@@ -0,0 +1,42 @@
+let shareTimeout = null;
+
+function setupSharing() {
+  const shareButtons = document.getElementsByClassName("share-button");
+  for (const shareButton of shareButtons) {
+    shareButton.addEventListener("click", onShareButtonClicked);
+  }
+}
+
+async function onShareButtonClicked(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const shareData = {
+    title: "VGTunes",
+    text: this.attributes["data-share-text"].value,
+    url: this.attributes["data-share-url"].value,
+  };
+
+  if (navigator.share) {
+    await navigator.share(shareData);
+  } else if (navigator.clipboard) {
+    await navigator.clipboard.writeText(shareData.url);
+    showCopiedToClipboardToast();
+  }
+}
+
+function showCopiedToClipboardToast() {
+  if (shareTimeout !== null) {
+    window.clearTimeout(shareTimeout);
+  }
+
+  const toastElement = document.getElementById("copied-to-clipboard-toast");
+  toastElement.style.setProperty("visibility", "visible");
+  shareTimeout = window.setTimeout(() => {
+    toastElement.style.setProperty("visibility", "hidden");
+  }, 3000);
+}
+
+(function () {
+  setupSharing();
+})();
