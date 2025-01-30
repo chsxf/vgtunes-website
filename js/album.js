45cce19455b8066795017e669cb68220c66dc40a
Christophe SAUVEUR <chsxf.pro@gmail.com>
2025-01-30 23:01:18 +0100

    ♻️ Moved analytics to localStorage

--- js/album.js
+++ js/album.js
@@ -1,16 +1,1 @@
-function setupBackLinks() {
-  if (document.referrer) {
-    const currentHost = document.location.host;
-    const refererrerURL = new URL(document.referrer);
-    if (refererrerURL.host == currentHost) {
-      const backButtonElements = document.getElementsByClassName("back-button");
-      for (let button of backButtonElements) {
-        button.classList.remove("hidden");
-      }
-    }
-  }
-}
-
-(function () {
-  setupBackLinks();
-})();
+function setupBackLinks(){if(document.referrer){const currentHost=document.location.host;const refererrerURL=new URL(document.referrer);if(refererrerURL.host==currentHost){const backButtonElements=document.getElementsByClassName("back-button");for(let button of backButtonElements){button.classList.remove("hidden");}}}}(function(){setupBackLinks();})();
